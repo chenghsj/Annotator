e78fd6ef21b7dd6ecc3491b9cb4450ce0ecdb55f
Cheng <chengjohnsonhs@gmail.com>
2022-05-19 01:28:17 +0800

    store data

--- inject/inject.js
+++ inject/inject.js
@@ -7,74 +7,66 @@
 var selectedText, bookmarkList;
 
 function getAllStorageLocalData() {
-  return new Promise((resolve, reject) => {
-    chrome.storage.local.get(null, (items) => {
-      if (chrome.runtime.lastError) {
-        return reject(chrome.runtime.lastError);
-      }
-      resolve(items);
-    });
-  });
+	return new Promise((resolve, reject) => {
+		chrome.storage.local.get(null, (items) => {
+			if (chrome.runtime.lastError) {
+				return reject(chrome.runtime.lastError);
+			}
+			resolve(items);
+		});
+	});
 }
 
 function encodeString(str) {
-  return [...new TextEncoder().encode(str)];
+	return [...new TextEncoder().encode(str)];
 }
-
-// async function cloneObject(args) {
-//   funcSrc = chrome.runtime.getURL("helper/cloneFunction.js");
-//   fun = await import(funcSrc);
-//   return fun.cloneObject(args);
-// }
 
 async function bookMarkInit() {
-  let items = await getAllStorageLocalData();
-  bookmarkList = items.bookmarkList || [];
-  console.log(bookmarkList);
+	let items = await getAllStorageLocalData();
+	bookmarkList = items.bookmarkList || [];
+	console.log("Get Storage Data: ", bookmarkList);
 }
 
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  if (message.addBookMark) {
-    console.log(message.tab);
-    let tab = message.tab;
-    let firstTenEncodedContent, lastTenEncodedContent;
-    let firstTenEncodedURL, lastTenEncodedURL;
-    let encodedContent,
-      encodedContents = [],
-      encodedURLs = [];
-    let encodedSelectedContent = encodeString(window.getSelection().anchorNode.parentElement.innerText);
-    let encodedURL = [encodeString(tab.url).slice(0,10), encodeString(tab.url).slice(0,10)];
+chrome.runtime.onMessage.addListener(async function (message, sender, sendResponse) {
+	let items = await getAllStorageLocalData();
+	bookmarkList = items.bookmarkList || [];
+	if (message.addBookMark) {
+		let tab = message.tab;
+		let bookmarkIdx;
+		let encodedContent;
+		let encodedSelectedContent = encodeString(
+			window.getSelection().anchorNode.parentElement.innerText
+		);
+		console.log(message.tab);
 
-    firstTenEncodedContent = encodedSelectedContent.slice(0, 10).join("");
-    lastTenEncodedContent = encodedSelectedContent.slice(-10).join("");
+		encodedContent = [
+			encodedSelectedContent.slice(0, 10).join(""),
+			encodedSelectedContent.slice(-10).join(""),
+		].join("");
 
-    firstTenEncodedURL = encodedURL.slice(0, 10).join("");
-    lastTenEncodedURL = encodedURL.slice(-10).join("");
+		bookmarkIdx = bookmarkList.findIndex((item) => item.url === tab.url);
+		if (bookmarkIdx >= 0) {
+			if (bookmarkList[bookmarkIdx].markList.indexOf(encodedContent) < 0) {
+				bookmarkList[bookmarkIdx].markList.push(encodedContent);
+			}
+		} else {
+			bookmarkList.push({
+				url: tab.url,
+				markList: [encodedContent],
+			});
+		}
 
-    encodedContents.push(firstTenEncodedContent, lastTenEncodedContent);
-    encodedURLs.push(firstTenEncodedURL, lastTenEncodedURL);
-    console.log(encodedContents);
-    bookmarkList.forEach((item) => {
-      if (item.encodedURL === encodedURLs.join()) {
-        item.marks.indexOf(encodedContents.join(""));
-      }
-    });
-    bookmarkList.push({
-      encodedURL: encodedURLs.join(""),
-      marks: [encodedContents],
-    });
-    // [tab.url] = encodedArr;
-
-    chrome.storage.local.set({ bookmarkList });
-  }
-  sendResponse();
+		chrome.storage.local.set({ bookmarkList });
+		console.log("Save to Storage", bookmarkList);
+	}
+	sendResponse();
 });
 
 // chrome.storage.local.clear();
 
 window.addEventListener("keydown", function (e) {
-  if (e.ctrlKey && e.altKey && e.code === "KeyK") {
-  }
+	if (e.ctrlKey && e.altKey && e.code === "KeyK") {
+	}
 });
 
 bookMarkInit();
